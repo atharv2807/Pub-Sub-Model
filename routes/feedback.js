e1642f3da87ce230e79dbdd4d434adca564e44a8
Atharv Goel <agoel@deqode.com>
2023-03-20 19:21:48 +0530

    Resolved Bug: 'path not found'

--- routes/feedback.js
+++ routes/feedback.js
@@ -4,7 +4,7 @@ import path from 'path'
 const router=express.Router();
 
 router.post('/feedback',(req,res)=>{
-    res.sendFile(path.join(dirname,'/public/feedback.html'))
+    res.sendFile(path.join(dirname,'/public/success.html'))
 })
 
 export { router as postFeedback}
--- routes/unsubscribe.js
+++ routes/unsubscribe.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { unsubscribeUser } from '../controllers/unsubscribe.js';
 import path from 'path'
+import { dirname } from '../index.js';
 const router = express.Router();
 
 router.post('/unsubscribe',async (req,res)=>{
     const {email}=req.body;
     const response=await unsubscribeUser(email)
     if(response==='Successful'){
-        res.sendFile(path.join(__dirname,'/public/feedback.html'))
+        res.sendFile(path.join(dirname,'/public/feedback.html'))
     }
 
 })
